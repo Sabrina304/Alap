118a25a1502d11919fd92b92db5073fe80fe958f
Sabrina304 <tomaju304@gmail.com>
2023-03-05 09:05:51 +0600

    initial

---
+++ app/src/main/java/com/example/alap/DashboardActivity.java
@@ -0,0 +1,39 @@
+package com.example.alap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class DashboardActivity extends AppCompatActivity {
+    CardView articleCard;
+    CardView checkCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        articleCard = findViewById(R.id.articleCard);
+        articleCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ArticleActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        checkCard = findViewById(R.id.checkCard);
+        checkCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CheckyourselfActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
